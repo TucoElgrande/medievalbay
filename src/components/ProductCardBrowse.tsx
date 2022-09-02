71ecca292277494388b285354f1e1bd8b62318ed
Serover <35572082+Serover@users.noreply.github.com>
2022-09-02 13:56:32 +0200

    name change

--- src/components/ProductCardBrowse.tsx
+++ src/components/ProductCardBrowse.tsx
@@ -3,13 +3,13 @@ import { useCart } from "../context/CartContext";
 import { Product } from "../data/data";
 
 function ProductCardBrowse(product: Product) {
-    const { addToCart, removeFromCart } = useCart();
+    const { addToCart, removeOneFromCart } = useCart();
 
     return (
         <div key={product.id}>
             <h3>{product.title}</h3>
             <button onClick={() => addToCart(product)}> Add to cart</button>
-            <button onClick={() => removeFromCart(product)}> Remove to cart</button>
+            <button onClick={() => removeOneFromCart(product)}> Remove to cart</button>
             <br></br>
             <img src={product.imageUrl} />
         </div>
