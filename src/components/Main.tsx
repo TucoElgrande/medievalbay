cb9a26cceaa466e9dac78a96c38932fdb4b94c5b
CmdrMaylee <meleechan@gmail.com>
2022-09-02 11:15:56 +0200

    Remove ALL products from cart

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,21 +3,22 @@ import { useCart } from "../context/CartContext";
 import mockData from "../data/data";
 
 function Main() {
-  const [product] = useState(mockData);
-  const { addToCart } = useCart();
+    const [product] = useState(mockData);
+    const { addToCart, removeFromCart } = useCart();
 
-  return (
-    <main>
-      {product.map((product) => (
-        <div key={product.id}>
-          <h3>{product.title}</h3>
-          <button onClick={() => addToCart(product)}> Add to cart</button>
-          <br></br>
-          <img src={product.imageUrl} />
-        </div>
-      ))}
-    </main>
-  );
+    return (
+        <main>
+            {product.map((product) => (
+                <div key={product.id}>
+                    <h3>{product.title}</h3>
+                    <button onClick={() => addToCart(product)}> Add to cart</button>
+                    <button onClick={() => removeFromCart(product)}> Remove to cart</button>
+                    <br></br>
+                    <img src={product.imageUrl} />
+                </div>
+            ))}
+        </main>
+    );
 }
 
 export default Main;
--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -2,37 +2,38 @@ import React, { createContext, ReactNode, useContext, useState } from "react";
 import { Product } from "../data/data";
 
 interface ContextValue {
-  cart: Product[];
-  addToCart: (product: Product) => void;
-  removeFromCart: (product: Product) => void;
+    cart: Product[];
+    addToCart: (product: Product) => void;
+    removeFromCart: (product: Product) => void;
 }
 
 interface Props {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 const CartContext = createContext<ContextValue>({
-  cart: [],
-  addToCart: () => {},
-  removeFromCart: () => {},
+    cart: [],
+    addToCart: () => {},
+    removeFromCart: () => {},
 });
 
 function CartProvider({ children }: Props) {
-  const [cart, setCart] = useState<Product[]>([]);
+    const [cart, setCart] = useState<Product[]>([]);
 
-  const addToCart = (product: Product) => {
-    setCart((prevState) => [...prevState, product]);
-    console.log(cart);
-  };
-  const removeFromCart = (product: Product) => {
-    //TODO add implementation
-  };
+    const addToCart = (product: Product) => {
+        setCart((prevState) => [...prevState, product]);
+        console.log(cart);
+    };
+    const removeFromCart = () => {
+        setCart((prevState) => [...prevState]);
+        cart.splice(0);
+    };
 
-  return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
-      {children}
-    </CartContext.Provider>
-  );
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+            {children}
+        </CartContext.Provider>
+    );
 }
 export const useCart = () => useContext(CartContext);
 
