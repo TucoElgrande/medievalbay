feb1ab25e0f78b7a3e104e93aed7a2df7f93711a
OdiousCode <gorgande@gmail.com>
2022-09-02 13:30:57 +0200

    #20 Added simple thank you alert on purchase

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+import Main from "../components/Main";
+import Button from '@mui/material/Button';
+
 
 function Checkout() {
-    return <h2>Checkout</h2>;
+    const thankYou = () => {
+        alert("Thanks for your purchase.");
+    }
+    return (
+    <div>
+        <h2>Checkout</h2>
+        <Main></Main>
+        <Button variant="contained" size="large" onClick={thankYou}>Buy now</Button>
+
+    </div>
+    );
 }
 
 export default Checkout;
