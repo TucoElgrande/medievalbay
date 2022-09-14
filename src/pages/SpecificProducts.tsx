91c9b08307659c3ff477774fbe929fb0f5a96cdc
CmdrMaylee <meleechan@gmail.com>
2022-09-14 11:02:14 +0200

    Fixed specific product bg & hightened media-query

--- src/pages/SpecificProducts.tsx
+++ src/pages/SpecificProducts.tsx
@@ -4,7 +4,7 @@ import { getAmountOfProducts, getProduct } from "../data/data";
 import ProductCardBrowse from "../components/ProductCardBrowse";
 import "./Products.css";
 import { useProduct } from "../context/ProductContext";
-import { Button } from "@mui/material";
+import { Button, Card, CardContent, Typography } from "@mui/material";
 import { useCart } from "../context/CartContext";
 import { currencyFormat } from "../utilities/currencyFormat";
 import ProductNotFound from "../components/ProductNotFound";
@@ -19,39 +19,36 @@ function SpecificProducts() {
         return <ProductNotFound errorMessage={"Merlin could not find this product"} />;
     } else {
         return (
-            <div className="specific-products-main center-non-flex">
-                <div>
-                    <div className="flex-container flex-wrap products-main center-items center-non-flex">
-                        <h2>{product.title}</h2>
-                    </div>
-
-                    <div className="flex-container flex-wrap products-main center-items center-non-flex">
-                        <img src={product.imageUrl} className="specific-product-browse-img" />
-                    </div>
-
-                    <div className="flex-container flex-wrap products-main center-items center-non-flex">
-                        <p className="products-main"> {product.description} </p>
-                    </div>
-
-                    <div className="flex-container flex-wrap products-main center-items center-non-flex">
-                        <p> {currencyFormat(product.price)}</p>
-                    </div>
-
-                    <div className="flex-container flex-wrap products-main center-items center-non-flex">
-                        <Button
-                            variant="contained"
-                            size="medium"
-                            onClick={() => addToCart(product)}
-                        >
-                            Add to cart
-                        </Button>
-                    </div>
+            <div className="main-content center-non-flex flex-container center-items flex-direction-column">
+                <div className="flex-container center-items">
+                    <Card sx={{ maxWidth: "60%", margin: 2 }} key={product.id}>
+                        <CardContent className="flex-container center-items flex-direction-column">
+                            <h2>{product.title}</h2>
+                            <img src={product.imageUrl} className="specific-product-browse-img" />
+                            <p> {product.description} </p>
+                            <p> {currencyFormat(product.price)}</p>
+                            <Button
+                                variant="contained"
+                                size="medium"
+                                onClick={() => addToCart(product)}
+                            >
+                                Add to cart
+                            </Button>
+                        </CardContent>
+                    </Card>
                 </div>
 
-                <div className="flex-container flex-wrap products-main center-items center-non-flex">
-                    <h3>Related Products</h3>
+                <div className="flex-item-two text text-align-center">
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4" component="div">
+                                Related products
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </div>
-                <div className="flex-container flex-wrap products-main center-items center-non-flex">
+
+                <div className="flex-container flex-wrap center-items products-main center-non-flex">
                     {relatedProducts.map((product) => (
                         <div className="product-margin">
                             <ProductCardBrowse
